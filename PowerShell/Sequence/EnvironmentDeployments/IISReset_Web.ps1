9035ef4fd4293ecc314df3799145a48f1f826246
briandonnelly2 <77056041+briandonnelly2@users.noreply.github.com>
2024-11-07 14:37:23 +0000

    Clearing up repo

--- PowerShell/Sequence/EnvironmentDeployments/IISReset_Web.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location -Path $PSScriptRoot | Out-Null
-
-if (-Not (Test-Path "Logs")) {
-    New-Item -ItemType directory -Path "Logs" | Out-Null
-}
-
-$runTime = (Get-Date -format "yyyy-MM-dd_HH-mm-ss")
-
-.\Scripts\IisReset.ps1 -ServerType Web | tee (".\Logs\" + $runTime + "_IisReset_Web.txt")
--- PowerShell/Sequence/EnvironmentDeployments/Setup/PrepareWorkflowEnvironment-Web/README.md
+++
@@ -1,2 +0,0 @@
-# Prepare Workflow Web Environment Task
-This task is used to set up a Workflow web Environment. Although built to potentially work as a VSTS task, there is currently no plan to expose it as one.
